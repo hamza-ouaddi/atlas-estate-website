40bcf933399944c4b5f584429f08293058235f3e
hamza-ouaddi <hamza0addi@gmail.com>
2023-11-16 14:47:41 +0100

    make hero section with navbar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,16 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      black: "#1f1f27",
-      blue: "#000dff",
-      orange: "#f97432",
-      blueGradient: "linear-gradient(135deg, #6b73ff 0%, #000dff 100%)",
-      orangeGradient: "linear-gradient(135deg, #fec163 0%, #de4313 100%)",
-      darkBackground: "linear-gradient(104deg, #0051d6 0.01%, #030118 100%)",
-      grey: "#bababc",
-      lightBlue: "#e6e7ff",
+    extend: {
+      colors: {
+        black: "#1f1f27",
+        blue: "#000dff",
+        orange: "#f97432",
+        blueGradient: "linear-gradient(135deg, #6b73ff 0%, #000dff 100%)",
+        orangeGradient: "linear-gradient(135deg, #fec163 0%, #de4313 100%)",
+        darkBackground: "linear-gradient(104deg, #0051d6 0.01%, #030118 100%)",
+        grey: "#bababc",
+        lightBlue: "#e6e7ff",
+        white: "#f5f5f5",
+      },
     },
   },
   plugins: [],
