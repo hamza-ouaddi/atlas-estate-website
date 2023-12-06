d9ba82d4f22c20517f6d6b4a23333238e9021b1d
hamza-ouaddi <hamza0addi@gmail.com>
2023-12-06 15:51:51 +0100

    fix deployment issues

---
+++ client/config/config.js
@@ -0,0 +1,11 @@
+export const BASE_URL = "https://atlas-estate-server.vercel.app/api";
+
+//Auth0 Credentials
+export const AUTH_DOMAIN = "dev-8xg3ulyhiwa1pxo0.us.auth0.com";
+export const AUTH_CLIENT_ID = "ztrz2jH1ZORAmAnDNzqf0oEtg3dNo3eO";
+export const REDIRECT_URI = "https://atlas-estate.vercel.app";
+export const AUTH_AUDIENCE = "http://localhost:8000";
+
+//Cloudinary Upload Credentials
+export const UPLOAD_CLOUD_NAME = "dyoet0pig";
+export const UPLOAD_PRESET = "uvjdua67";
